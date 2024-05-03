7165f421fd7ed643cf9f9b19cc727456c3c03b34
Pablo <pablo.magarinos@outlook.com>
2024-05-04 00:12:46 +0200

    Update test_sat.py

--- test_sat.py
+++ test_sat.py
@@ -6,18 +6,18 @@ sys.path.append('src/')
 
 import sat_env
 
-satelite = sat_env.Satellite(I = 1, ang_position = 5, ang_velocity = 0)
+satelite = sat_env.Satellite(I = 1, ang_position = 0, ang_velocity = 0.2)
 sim = sat_env.Mechanics(satelite, dt = 0.1)
 
 status = {"ang_velocity": [], "ang_position": []}
 status["time"] = np.arange(0, 10, 0.1)
+
 
 for i in range(len(status["time"])):
     torque = 0.1
     sim.update(torque)
     status["ang_velocity"].append(satelite.ang_velocity)
     status["ang_position"].append(satelite.ang_position)
-
 
 # Crear una figura y un eje polar
 fig = plt.figure()
