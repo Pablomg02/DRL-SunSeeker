b9763aab4e06b664f6bd66602c1e03d1afb806e8
Pablo <pablo.magarinos@outlook.com>
2024-05-03 20:40:08 +0200

    Update satellite.py

--- src/sat_env/satellite.py
+++ src/sat_env/satellite.py
@@ -7,11 +7,18 @@ class Satellite():
                  ang_velocity : float = 0):
         
         self.I = I # Inertia [kg*m^2]
+
+        if ang_position < 0 or ang_position > 2*np.pi:
+            raise ValueError('Initial angular position must be between 0 and 2*pi')
         self.ang_velocity = ang_velocity # Angular velocity [rad/s]
         self.ang_position = ang_position # Angular position [rad]
 
     def __str__(self) -> str:
-        return f'Satellite(Inertia: {self.I}, Angular Position: {self.ang_position}, Angular Velocity: {self.ang_velocity})'
+        return f'Satellite(Inertia: {self.I}, Angular Position: {self.ang_position} ({self.ang_position_deg} deg), Angular Velocity: {self.ang_velocity})'
+    
+    @property
+    def ang_position_deg(self):
+        return np.rad2deg(self.ang_position)
 
 
     def reset(self):
@@ -21,4 +28,4 @@ class Satellite():
 
     def random_reset(self):
         self.ang_velocity = random.uniform(-1, 1)
-        self.ang_position = random.uniform(0, 1)
+        self.ang_position = random.uniform(0, np.pi*2)
